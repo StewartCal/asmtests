96c24f727fc8d56749116516f5f1f7f8375fb75d
Cal Stewart <castewart@tntech.edu>
2026-04-27 11:57:48 -0500

    c

--- substring.asm
+++ substring.asm
@@ -1,8 +1,6 @@
 section .text
 global find_substring
 
-; void find_substring(char *main_str, char *substr, int *starting_index)
-
 find_substring:
     push ebp
     mov ebp, esp
@@ -11,52 +9,54 @@ find_substring:
     push esi
     push edi
 
-    mov esi, [ebp+8]      ; esi = main_str base address
-    mov edi, [ebp+12]     ; edi = substr base address
-    mov edx, [ebp+16]     ; edx = &starting_index
+    mov esi, [ebp+8]      ; main_str
+    mov edi, [ebp+12]     ; substr
+    mov edx, [ebp+16]     ; &starting_index
 
-    mov ecx, 0            ; ecx = main_index
+    mov ecx, 0            ; i = 0
 
-check_start_position:
-    mov al, [esi + ecx]   ; al = main_str[main_index]
+check_start:
+    mov al, [esi + ecx]
     cmp al, 0
-    je substring_not_found
+    je not_found
 
-    mov eax, ecx          ; eax = saved starting index
-    mov ebx, 0            ; ebx = substring_index
+    mov eax, ecx          ; save i
+    mov ebx, 0            ; j = 0
 
-compare_at_current_start:
-    mov dl, [edi + ebx]        ; dl = substr[substring_index]
+compare_loop:
+    mov dl, [edi + ebx]   ; substr[j]
     cmp dl, 0
-    je substring_found
+    je found
 
-    mov dh, [esi + ecx + ebx]  ; dh = main_str[main_index + substring_index]
+    mov eax, ecx
+    add eax, ebx          ; eax = i + j
+    mov dh, [esi + eax]   ; main[i + j]
+
     cmp dh, 0
-    je substring_not_found
+    je not_found
 
     cmp dh, dl
-    jne move_to_next_start
+    jne next_start
 
     inc ebx
-    jmp compare_at_current_start
+    jmp compare_loop
 
-move_to_next_start:
+next_start:
     inc ecx
-    jmp check_start_position
+    jmp check_start
 
-substring_found:
-    mov edx, [ebp+16]
-    mov [edx], eax
+found:
+    mov eax, [ebp+16]
+    mov [eax], ecx
     jmp done
 
-substring_not_found:
-    mov edx, [ebp+16]
-    mov dword [edx], -1
+not_found:
+    mov eax, [ebp+16]
+    mov dword [eax], -1
 
 done:
     pop edi
     pop esi
     pop ebx
-
     pop ebp
     ret
